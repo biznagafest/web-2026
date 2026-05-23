44b3e9f9d2e08cbfb92991c22b74eddd21a8b582
David Rojo <davidrojo1997@gmail.com>
2026-05-23 20:05:04 +0200

    feat: update 2026 style

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://biznagafest.com",
   integrations: [preact()],
   output: "server",
   adapter: node({ mode: "standalone" }),
